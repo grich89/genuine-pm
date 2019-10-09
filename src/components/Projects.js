d6721744f2c3292c8b1d4ad175bb55925dfb7f7a
Greg Rich <grich@wearegenuine.com>
2019-10-09 10:40:25 -0400

    try to add headers; send to patrick

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
-
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 class Projects extends Component {
+    state = {
+        todos: []
+    }
+    componentDidMount() {
+        fetch('https://hackathon20191007122027.azurewebsites.net/api/data/alljiraprojects')
+        .then(res => res.text())
+        .then((data) => {
+          this.setState({ todos: data })
+          console.log(this.state.todos)
+        })
+        .catch(console.log)
+    }
     constructor(props) {
         super(props);
         this.state = {}
@@ -8,7 +21,7 @@ class Projects extends Component {
     render() {
         return (
             <div className="container">
-                <h1>Projects</h1>
+                <h1>Projects!</h1>
             </div>
         );
     }
